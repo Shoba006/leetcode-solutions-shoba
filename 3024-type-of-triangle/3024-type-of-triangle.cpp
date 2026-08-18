7b27c71b4506922e6024729d0877fc0670a617ca
Shoba006 <shobamalika006@gmail.com>
2026-08-19 01:51:27 +0530

    Time: 0 ms (100%), Space: 22.9 MB (68.12%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string triangleType(vector<int>& n) {
+
+        if(n[0]+n[1]<=n[2] ||
+           n[1]+n[2]<=n[0] ||
+           n[0]+n[2]<=n[1]){
+            return "none";
+        }
+        else if(n[0]==n[1] && n[1]==n[2]){
+            return "equilateral";
+        }
+        else if(n[0]==n[1] && n[1]!=n[2] ||
+        n[0]==n[2] && n[2]!=n[1] || 
+        n[1]==n[2] && n[2]!=n[0]){
+            return "isosceles";
+        }
+        else{
+            return "scalene";
+        }
+
+    }
+};

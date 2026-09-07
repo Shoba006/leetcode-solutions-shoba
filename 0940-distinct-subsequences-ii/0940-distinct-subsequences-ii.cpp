83d5c97408b7d2777760bb49b399ae93e0758791
Shoba006 <shobamalika006@gmail.com>
2026-09-08 01:13:06 +0530

    Time: 0 ms (100%), Space: 9.8 MB (22.91%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int MOD = 1e9 + 7;
+
+        vector<long long> dp(s.size() + 1, 0);
+        dp[0] = 1;
+
+        vector<long long> last(26, 0);
+
+        for (int i = 1; i <= s.size(); i++) {
+            int ch = s[i - 1] - 'a';
+
+            dp[i] = (2 * dp[i - 1]) % MOD;
+
+            dp[i] = (dp[i] - last[ch] + MOD) % MOD;
+
+            last[ch] = dp[i - 1];
+        }
+
+        return (dp[s.size()] - 1 + MOD) % MOD;
+    }
+};
